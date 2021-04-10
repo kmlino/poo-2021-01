e7bd78fa77eb57a00560810cca1045360d1254cc
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-04-09 22:05:08 -0300

    Acrescenta o exercício 02

--- 08-exercicios-scenebuilder/src/main/java/br/edu/facthus/scenebuilder/App.java
+++ 08-exercicios-scenebuilder/src/main/java/br/edu/facthus/scenebuilder/App.java
@@ -19,7 +19,8 @@ public class App extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// scene = new Scene(loadFXML("main"), 640, 480);
-		scene = new Scene(loadFXML("exercicio01"), 640, 480);
+		// scene = new Scene(loadFXML("exercicio01"), 640, 480);
+		scene = new Scene(loadFXML("exercicio02"), 640, 480);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
