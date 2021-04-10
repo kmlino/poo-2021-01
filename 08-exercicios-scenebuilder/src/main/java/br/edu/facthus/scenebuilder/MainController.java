763ad30e74b0400b6b25819497315eadd05ff7ac
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-04-09 22:33:36 -0300

    Acrescenta o exercício 03

--- 08-exercicios-scenebuilder/src/main/java/br/edu/facthus/scenebuilder/MainController.java
+++ 08-exercicios-scenebuilder/src/main/java/br/edu/facthus/scenebuilder/MainController.java
@@ -1,21 +1,43 @@
 package br.edu.facthus.scenebuilder;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class MainController {
 	
-	@FXML
-	private TextField txtEntrada;
-	
-	@FXML
-	private Label lblSaida;
-	
-	@FXML
-	private void teste() {
-		lblSaida.setText("Olá " + txtEntrada.getText() 
-		 + "! Seja bem vindo!");
+	private void mostraExercicio(String nomeFxml, String titulo) {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			fxmlLoader.setLocation(getClass().getResource(nomeFxml));
+			Scene scene = new Scene(fxmlLoader.load(), 630, 400);
+			Stage stage = new Stage();
+			stage.setTitle(titulo);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	@FXML
+	private void ex01() {
+		mostraExercicio("exercicio01.fxml", "Exercício 01");
+	}
+	
+	@FXML
+	private void ex02() {
+		mostraExercicio("exercicio02.fxml", "Exercício 02");
+	}
+	
+	@FXML
+	private void ex03() {
+		mostraExercicio("exercicio03.fxml", "Exercício 031");
+	}
+	
+	
+
 }
