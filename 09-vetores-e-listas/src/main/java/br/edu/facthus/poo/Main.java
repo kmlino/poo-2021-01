7b48a9feaab7bee6077e07daf64c6e7af42a8169
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-04-14 20:29:29 -0300

    Acrescenta o exercício mesmosElementos

--- 09-vetores-e-listas/src/main/java/br/edu/facthus/poo/Main.java
+++ 09-vetores-e-listas/src/main/java/br/edu/facthus/poo/Main.java
@@ -81,13 +81,44 @@ public class Main {
 		for (String elemento : lista)
 			System.out.println(elemento);
 	}
+	
+	// Retorna:
+	// true - se vetor e lista têm os mesmos elementos
+	// false - caso contrário
+	// Considerar que não há elementos repetidos
+	public static boolean mesmosElementos(Integer[] vetor,
+			ArrayList<Integer> lista) {
+		// Se tamanhos diferentes, retorna false
+		if (vetor.length != lista.size())
+			return false;
+		
+		for (Integer i : vetor) {
+			if (!lista.contains(i))
+				return false;
+		}
+		
+		return true;
+	}
+	
+	public static void exercicio01() {
+		Integer[] v = {2, 1, 3};
+		
+		ArrayList<Integer> l = new ArrayList<>();
+		l.add(1);
+		l.add(2);
+		l.add(3);
+		
+		System.out.println(mesmosElementos(v, l));
+	}
 
 	public static void main(String[] args) {
 		// exemplo01();
 		// exemplo02();
 		// exemplo03();
 		// exemplo04();
-		exemplo05();
+		// exemplo05();
+		
+		exercicio01();
 		
 
 	}
