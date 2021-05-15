84c9c1649bf12dffe7d4f8ea2ed563b67a9cc0ba
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-05-14 22:24:33 -0300

    Acrescenta a tabela de corretores

--- 10-relacionamento-objetos/src/main/java/br/edu/facthus/gui/CorretoresController.java
+++ 10-relacionamento-objetos/src/main/java/br/edu/facthus/gui/CorretoresController.java
@@ -1,12 +1,21 @@
 package br.edu.facthus.gui;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 import br.edu.facthus.db.DbTeste;
 import br.edu.facthus.model.Corretor;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class CorretoresController {
+public class CorretoresController implements Initializable {
 	
 	@FXML
 	private TextField txtNome;
@@ -27,6 +36,21 @@ public class CorretoresController {
 	private Label statusLabel;
 	
 	@FXML
+	private TableView<Corretor> tblCorretores;
+	
+	@FXML
+	private TableColumn<Corretor, String> colNome;
+	
+	@FXML
+	private TableColumn<Corretor, String> colCpf;
+	
+	@FXML
+	private TableColumn<Corretor, String> colTelefone;
+	
+	@FXML
+	private TableColumn<Corretor, String> colCresci;
+	
+	@FXML
 	private void cadastra() {
 		Corretor corretor = new Corretor(txtNome.getText(),
 				txtCpf.getText(),
@@ -38,6 +62,26 @@ public class CorretoresController {
 		DbTeste.imprimeCorretores();
 		
 		statusLabel.setText("Corretor cadastrado com sucesso!");
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		colNome.setCellValueFactory(
+				new PropertyValueFactory<Corretor, String>("nome"));
+		colTelefone.setCellValueFactory(
+				new PropertyValueFactory<Corretor, String>("telefone"));
+		colCpf.setCellValueFactory(
+				new PropertyValueFactory<Corretor, String>("cpf"));
+		colCresci.setCellValueFactory(
+				new PropertyValueFactory<Corretor, String>("cresci"));
+		
+		ArrayList<Corretor> corretores = new ArrayList<>();
+		corretores.add(new Corretor("Fulano de Tal", "123", "1234", "111", .05));
+		corretores.add(new Corretor("Beltrano de Tal", "222", "333", "444", .05));
+		corretores.add(new Corretor("Cicrano de Tal", "555", "666", "777", .05));
+		
+		tblCorretores.setItems(
+				FXCollections.observableArrayList(corretores));
 	}
 	
 }
