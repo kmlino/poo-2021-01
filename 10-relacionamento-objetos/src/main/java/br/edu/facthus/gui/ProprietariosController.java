77e1467f80e586344def60c0b9388a23fe01bb0f
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-05-14 21:47:06 -0300

    Acrescenta a tabela de proprietários

--- 10-relacionamento-objetos/src/main/java/br/edu/facthus/gui/ProprietariosController.java
+++ 10-relacionamento-objetos/src/main/java/br/edu/facthus/gui/ProprietariosController.java
@@ -1,12 +1,21 @@
 package br.edu.facthus.gui;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 import br.edu.facthus.db.DbTeste;
 import br.edu.facthus.model.Pessoa;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class ProprietariosController {
+public class ProprietariosController implements Initializable {
 	
 	@FXML
 	private TextField txtNome;
@@ -21,6 +30,18 @@ public class ProprietariosController {
 	private Label statusLabel;
 	
 	@FXML
+	private TableView<Pessoa> tblProprietarios;
+	
+	@FXML
+	private TableColumn<Pessoa, String> colNome;
+	
+	@FXML
+	private TableColumn<Pessoa, String> colCpf;
+	
+	@FXML
+	private TableColumn<Pessoa, String> colTelefone;
+	
+	@FXML
 	private void cadastra() {
 		Pessoa pessoa = new Pessoa(txtNome.getText(),
 				txtCpf.getText(),
@@ -30,6 +51,27 @@ public class ProprietariosController {
 		DbTeste.imprimeProprietarios();
 		
 		statusLabel.setText("Proprietário cadastrado com sucesso!");
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// Configura as factories
+		colNome.setCellValueFactory(
+				new PropertyValueFactory<Pessoa, String>("nome"));
+		colCpf.setCellValueFactory(
+				new PropertyValueFactory<Pessoa, String>("cpf"));
+		colTelefone.setCellValueFactory(
+				new PropertyValueFactory<Pessoa, String>("telefone"));
+		
+		// Cria proprietários de teste
+		ArrayList<Pessoa> proprietarios = new ArrayList<>();
+		proprietarios.add(new Pessoa("João Silva", "123.456.789-10", "(34) 3322-2233"));
+		proprietarios.add(new Pessoa("Maria Souza", "987.654.321-00", "(34) 3322-2222"));
+		proprietarios.add(new Pessoa("José Silva", "111.222.333-44", "(34) 3322-3333"));
+		
+		// Vincula a lista à tabela
+		tblProprietarios.setItems(
+				FXCollections.observableArrayList(proprietarios));
 	}
 	
 }
